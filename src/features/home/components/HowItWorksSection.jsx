b2cbdf50e6e9688bf059da5fe40a81e7dcdbbfe6
myxzlpltk <myxzlpltk@gmail.com>
2022-09-25 10:33:21 +0700

    update UI how it works

--- src/features/home/components/HowItWorksSection.jsx
+++ src/features/home/components/HowItWorksSection.jsx
@@ -2,39 +2,52 @@ import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {setActiveMethod} from "../../../redux/reducers/home";
 import MethodCard from "./MethodCard";
+import SimpleMethodCard from "./SimpleMethodCard";
 
 const HowItWorksSection = () => {
-	const images = useSelector(state => state.home.images);
-	const activeMethod = useSelector(state => state.home.activeMethod);
-	const dispatch = useDispatch();
+  const images = useSelector(state => state.home.images);
+  const activeMethod = useSelector(state => state.home.activeMethod);
+  const dispatch = useDispatch();
 
-	return (
-		<section className="bg-how-it-works bg-cover bg-no-repeat text-white" id="how-it-works">
-			<div className="backdrop-blur-2xl">
-				<div className="container mx-auto py-20 px-10">
-					<h2 className="text-4xl font-bold text-center mb-2">Bagaimana Cara Kerjanya?</h2>
-					<h5 className="font-light text-center mb-20">
-						Kami memeriksa dokumen anda berdasarkan berbagai macam manipulasi.
-					</h5>
-					<div className="flex flex-wrap">
-						<div className="basis-5/12 pt-10">
-							<MethodCard title="Teks Ilusif" description="Mengubah warna teks menjadi putih seolah-olah spasi"
-													active={activeMethod === 0} onClick={() => dispatch(setActiveMethod(0))} />
-							<MethodCard title="Gambar Paragraf" description="Mengubah bagian paragraf menjadi gambar"
-													active={activeMethod === 1} onClick={() => dispatch(setActiveMethod(1))} />
-							<MethodCard title="Teks Tersembunyi" description="Menyembunyikan teks spam dibalik gambar"
-													active={activeMethod === 2} onClick={() => dispatch(setActiveMethod(2))} />
-							<MethodCard title="Font Palsu" description="Menggunakan font palsu untuk mengubah teks"
-													active={activeMethod === 3} onClick={() => dispatch(setActiveMethod(3))} />
-						</div>
-						<div className="basis-7/12">
-							<img src={images[activeMethod]} alt="Metode" className="max-w-full rounded-xl" />
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  const data = [
+    {title: "Teks Ilusif", description: "Mengubah warna teks menjadi putih seolah-olah spasi"},
+    {title: "Gambar Paragraf", description: "Mengubah bagian paragraf menjadi gambar"},
+    {title: "Teks Tersembunyi", description: "Menyembunyikan teks spam dibalik gambar"},
+    {title: "Font Palsu", description: "Menggunakan font palsu untuk mengubah teks"},
+  ]
+
+  return (
+    <section className="bg-how-it-works bg-cover bg-no-repeat text-white" id="how-it-works">
+      <div className="backdrop-blur-2xl">
+        <div className="container mx-auto py-20 px-10">
+          <h2 className="text-4xl font-bold text-center mb-2">Bagaimana Cara Kerjanya?</h2>
+          <h5 className="font-light text-center mb-10 lg:mb-20">
+            Kami memeriksa dokumen anda berdasarkan berbagai macam manipulasi.
+          </h5>
+          <div className="flex flex-wrap">
+            <div className="basis-5/12 pt-10 hidden lg:block">
+              {data.map((item, index) => (
+                <MethodCard title={item.title} description={item.description}
+                            active={activeMethod === index} onClick={() => dispatch(setActiveMethod(index))} />
+              ))}
+            </div>
+            <div className="basis-full block lg:hidden">
+              <div className="flex flex-wrap items-center justify-center mb-2">
+                {data.map((item, index) => (
+                  <SimpleMethodCard title={item.title} active={activeMethod === index}
+                                    onClick={() => dispatch(setActiveMethod(index))} />
+                ))}
+              </div>
+              <p className="text-center mb-4">{data[activeMethod].description}</p>
+            </div>
+            <div className="basis-full lg:basis-7/12">
+              <img src={images[activeMethod]} alt="Metode" className="max-w-full rounded-xl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HowItWorksSection;
---
+++ src/features/home/components/SimpleMethodCard.jsx
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import React from "react";
+
+const MethodCard = (props) => {
+	return (
+		<div
+			onClick={props.onClick}
+			className={classNames(
+				"card cursor-pointer inline-block m-1",
+				{
+					"bg-white/10 border border-white/20": props.active,
+					"hover:bg-white/5 border border-white/0": !props.active,
+				},
+			)}>
+			<div className="card-body px-4 py-1">
+				<h3 className="text-md font-bold">{props.title}</h3>
+			</div>
+		</div>
+	);
+};
+
+MethodCard.propTypes = {
+	title: PropTypes.string.isRequired,
+	active: PropTypes.bool,
+	onClick: PropTypes.func,
+};
+
+export default MethodCard;
