8779e8b6f0d4231ff8b78d0c947d896c229be15c
myxzlpltk <myxzlpltk@gmail.com>
2022-09-27 14:39:35 +0700

    is authenticated

--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -23,44 +23,48 @@ const Navbar = () => {
     <header className="bg-white sticky top-0 z-50">
       <div className="container mx-auto navbar py-4">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+          {isAuthenticated ? (
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link to="/dashboard">Dasbor</Link>
-              </li>
-            </ul>
-          </div>
+                <li>
+                  <Link to="/dashboard">Dasbor</Link>
+                </li>
+              </ul>
+            </div>
+          ) : null}
           <Link to="/" className="btn btn-ghost normal-case text-xl">
             <img src="/svg/logo.svg" alt="Logo" className="mr-2" />
             <span>{appName}</span>
           </Link>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <Link to="/dashboard">Dasbor</Link>
-            </li>
-          </ul>
-        </div>
+        {isAuthenticated ? (
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <Link to="/dashboard">Dasbor</Link>
+              </li>
+            </ul>
+          </div>
+        ) : null}
         <div className="navbar-end">
           {isAuthenticated ? (
             <GoogleLogout
